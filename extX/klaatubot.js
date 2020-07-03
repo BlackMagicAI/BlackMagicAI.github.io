883367cab32187404e06f38127cf3d081f70329d
mtedder <maurice_tedder@hotmail.com>
2020-07-02 21:50:23 -0400

    Klaatubot experimental scratchX extension.

---
+++ extX/klaatubot.js
@@ -0,0 +1,26 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    // send bluetooth command to klaatubot
+    ext.send_cmd = function() {
+            console.log("send_cmd")
+        };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+          // Block type, block name, function name
+          [' ', 'Klaatubot Send Cmd', 'send_cmd'],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('klaatubotExt', descriptor, ext);
+})({});
